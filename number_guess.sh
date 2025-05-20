3eaffb66893f4ed54daf8d3ba12065ccff7bad00
Shaik22175079 Asif <hpp3982@autuni.ac.nz>
2025-05-20 05:48:19 +0000

    feat: create number guessing logic

--- number_guess.sh
+++ number_guess.sh
@@ -3,6 +3,7 @@
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
 SECRET_NUMBER=$(( RANDOM % 1000 + 1 ))
+NUMBER_OF_GUESSES=0
 
 echo "Enter your username:"
 read USERNAME
@@ -11,9 +12,9 @@ USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME'")
 
 if [[ -z $USER_ID ]]
 then
-  echo "$($PSQL "INSERT INTO users(username) VALUES('$USERNAME')")"
-  echo "Welcome, $USERNAME! It looks like this is your first time here."
+  $PSQL "INSERT INTO users(username) VALUES('$USERNAME')" > /dev/null
   USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME'")
+  echo "Welcome, $USERNAME! It looks like this is your first time here."
 else
   GAMES_PLAYED=$($PSQL "SELECT COUNT(*) FROM games WHERE user_id=$USER_ID")
   BEST_GAME=$($PSQL "SELECT MIN(guesses) FROM games WHERE user_id=$USER_ID")
@@ -21,7 +22,6 @@ else
 fi
 
 echo "Guess the secret number between 1 and 1000:"
-NUMBER_OF_GUESSES=0
 
 while true
 do
@@ -37,8 +37,8 @@ do
 
   if [[ $GUESS -eq $SECRET_NUMBER ]]
   then
+    $PSQL "INSERT INTO games(user_id, guesses) VALUES($USER_ID, $NUMBER_OF_GUESSES)" > /dev/null
     echo "You guessed it in $NUMBER_OF_GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!"
-    $PSQL "INSERT INTO games(user_id, guesses) VALUES($USER_ID, $NUMBER_OF_GUESSES)"
     break
   elif [[ $GUESS -gt $SECRET_NUMBER ]]
   then
